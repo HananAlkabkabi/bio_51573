bc7655f379906969cd64285118349ed7f13bbfb7
Hanna Alkabkabi <hanan.kab0@gmail.com>
2026-04-14 22:17:31 -0500

    job positional, walltime optional

---
+++ scripts/my_funcations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/ env python3 
+
+##def greeting(name):
+    #print(f'hello, {name}')
+
+import my_funcations
+
+def main():
+    input_nmae = input("Enter a name: ")
+
+    my_funcations.greeting(input_nmae)
+
+
+
+#set the enviroment for this script 
+# is it the main(), 
+if __main == '__main__':
+    main()

74339b12ab70a22a96eaf843e9b902005f20f5f5
Hanna Alkabkabi <hanan.kab0@gmail.com>
2026-04-22 04:28:14 -0500

    fix main funcation

--- parse-GFF.py
+++ parse-GFF.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 
-#import gff_funcations.py    #call modual 
+
+import gff_functions   #call modual 
 
 #--------funcation to parse the command-line  
 def get_args():
@@ -9,10 +10,10 @@ def get_args():
     parser = argparse.ArgumentParser(description="This script to run fasta file and GFF file")
 
      #add a positional argument, fasta file
-    parser.add_argument("Fasta file", help="use input fast file", type=str)
+    parser.add_argument("fasta_file", help="use input fast file", type=str)
 
     #add a positional argument, GFF file
-    parser.add_argument("GFF file", help="use input fast file", type=str)
+    parser.add_argument("gff_file", help="use input gff file", type=str)
 
 
     # parse the arguments and return in two steps
@@ -20,7 +21,13 @@ def get_args():
     return args
 
 def main():
-      print("script works!")
+    args = get_args()
+
+    genome = gff_functions.read_fasta(args.fasta_file)
+    features = gff_functions.read_gff(args.gff_file, genome)
+    gff_functions.write_output(features)
+
+    print("Output written")
 
 # execute the program by calling main
 if __name__ == "__main__":
